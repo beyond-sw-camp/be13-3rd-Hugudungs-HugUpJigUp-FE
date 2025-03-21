70015791a58de7e80e161230634952fadaedf21f
DESKTOP-HALTBK4\Playdata <yjw127588@gmail.com>
2025-03-21 17:21:28 +0900

    feature/components/matching [유재우]: 매칭 코멘트 카드 컴포넌트(목업데이터 포함)

--- hugupjigup_FE/components.d.ts
+++ hugupjigup_FE/components.d.ts
@@ -7,8 +7,26 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    CommentsList: typeof import('./src/components/board/CommentsList.vue')['default']
+    CountLabel: typeof import('./src/components/profile/CountLabel.vue')['default']
+    CreateCommentForm: typeof import('./src/components/board/CreateCommentForm.vue')['default']
+    CreateMatchingCommentForm: typeof import('./src/components/matching/CreateMatchingCommentForm.vue')['default']
+    CreateMatchingForm: typeof import('./src/components/matching/CreateMatchingForm.vue')['default']
+    CreatePostForm: typeof import('./src/components/board/CreatePostForm.vue')['default']
+    Footer: typeof import('./src/components/layout/Footer.vue')['default']
+    Header: typeof import('./src/components/layout/Header.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    InsertTextForm: typeof import('./src/components/sign/InsertTextForm.vue')['default']
+    LogoTap: typeof import('./src/components/common/LogoTap.vue')['default']
+    MatchingCommentCard: typeof import('./src/components/matching/MatchingCommentCard.vue')['default']
+    MatchingPopup: typeof import('./src/components/matching/MatchingPopup.vue')['default']
+    MatchingPostCard: typeof import('./src/components/matching/MatchingPostCard.vue')['default']
+    MentorMenteeContext: typeof import('./src/components/profile/MentorMenteeContext.vue')['default']
+    PageTop: typeof import('./src/components/common/PageTop.vue')['default']
+    PostsList: typeof import('./src/components/board/PostsList.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    UpdateMentorMenteeForm: typeof import('./src/components/profile/UpdateMentorMenteeForm.vue')['default']
+    UpdateProfileForm: typeof import('./src/components/profile/UpdateProfileForm.vue')['default']
   }
 }
