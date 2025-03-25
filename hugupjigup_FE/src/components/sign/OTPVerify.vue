425b93e21cd8fa89bab2c8ab629f296af0c2127a
g00dbyul <skypower4203@gmail.com>
2025-03-25 17:48:20 +0900

    feat: sign up connect to backend

--- hugupjigup_FE/src/components/sign/OTPVerify.vue
+++ hugupjigup_FE/src/components/sign/OTPVerify.vue
@@ -8,16 +8,16 @@
         <h3 class="text-h6 mb-2">
           Please enter the one time password to verify your account
         </h3>
-  
+
         <div>A code has been sent to {{ maskedEmail }}</div>
-  
+
         <v-otp-input
           v-model="otp"
           :disabled="validating"
           color="primary"
           variant="plain"
         ></v-otp-input>
-  
+
         <v-btn
           :loading="validating"
           class="mt-6 text-none bg-surface-variant mx-auto"
@@ -33,55 +33,66 @@
       </v-card>
     </v-dialog>
   </template>
-  
+
   <script lang="ts" setup>
   import { ref, computed } from 'vue'
-  
+  import {signup, verificationOtp} from "@/usecases/user_usecase";
+
   const props = defineProps<{
     email: string
+    password: string
+    nickname: string
     modelValue: boolean
   }>()
-  
+
   const emit = defineEmits<{
     (e: 'update:modelValue', value: boolean): void;
     (e: 'otp-success'): void;
     (e: 'otp-fail'): void
   }>()
-  
+
   const dialogVisible = computed({
     get: () => props.modelValue,
     set: (val) => emit('update:modelValue', val)
   })
-  
+
   const otp = ref('')
   const validating = ref(false)
-  
+
   const maskedEmail = computed(() => {
     return '****@****' + props.email.slice(-4)
   })
 
   // OTP 검증 로직 (가상의 검증 로직 추가 가능)
-async function verifyOTP() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(otp.value === '123456') // 예제: '123456' 입력 시 성공
-    }, 2000)
-  })
+async function verifyOTP(): Promise<boolean> {
+    return  await verificationOtp({ email: props.email, otp: otp.value });
 }
-  
-  async function onClick() {
-    validating.value = true
 
-    const isValid = await verifyOTP()
-  
-    
-      validating.value = false
-      dialogVisible.value = false
-      if (isValid) {
-    emit('otp-success') // OTP 성공 이벤트 발생
-  } else {
-    emit('otp-fail') // OTP 실패 이벤트 발생
-  }
+  async function onClick() {
+    validating.value = true;
+
+    console.log(props.password);
+
+    const isValid: boolean = await verifyOTP();
+
+    validating.value = false
+    dialogVisible.value = false
+
+    if(!isValid) {
+      emit('otp-fail') // OTP 실패 이벤트 발생
+      return;
+    }
+
+    const signUpRequest: boolean = await signup({
+        email: props.email,
+        password: props.password,
+        nickname: props.nickname
+      });
+
+    if (signUpRequest) {
+      emit('otp-success') // OTP 성공 이벤트 발생
+    } else {
+      emit('otp-fail') // OTP 실패 이벤트 발생
+    }
   }
   </script>
-  
