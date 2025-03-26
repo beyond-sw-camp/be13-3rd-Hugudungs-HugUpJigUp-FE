94740a51d3e8152e052e0e1e94783c669e2a8c52
이성훈 <dgh0612321@naver.com>
2025-03-26 11:12:03 +0900

    [이성훈] feature/pages/CreateNoticePostPage : CreateNoticePostPage와 CreateFreePostPage 코드 수정 완료

--- hugupjigup_FE/src/pages/board/CreateNoticePostPage.vue
+++ hugupjigup_FE/src/pages/board/CreateNoticePostPage.vue
@@ -1,28 +1,23 @@
 <template>
-    <v-container fluid>
-      <!-- 상단 로고 영역 -->
-      <LogoTap/>
-      <NoticePageBreadcrumb/>
-  
-      <!-- 공지 게시판 작성 폼 -->
-      <v-row justify="center">
-        <v-col cols="12" md="8" lg="6">
-          <CreateNoticePostForm />
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
-  
-  <script lang="ts" setup>  
+  <v-container fluid>
+    <!-- 상단 로고 영역 -->
 
+    <!-- 공지 게시판 작성 폼 -->
+    <v-row justify="center">
 
-  import CreateNoticePostForm from '@/components/board/CreateNoticePostForm.vue';
-  import NoticePageBreadcrumb from '@/components/common/NoticePageBreadcrumb.vue';
-  
-  
-  </script>
-  
-  <style scoped>
-  /* 필요 시 스타일 추가 */
-  </style>
-  
+      <v-col cols="12" md="8" lg="6">
+      <NoticePageBreadcrumb />
+
+        <CreateNoticePostForm c />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+import CreateNoticePostForm from "@/components/board/CreatePostForm.vue";
+import NoticePageBreadcrumb from "@/components/common/NoticePageBreadcrumb.vue";
+</script>
+
+<style scoped>
+</style>
